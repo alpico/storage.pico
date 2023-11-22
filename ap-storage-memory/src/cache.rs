69916c8fc06cf35088b29e122c2c34fe9cf42d6d
Bernhard Kauer <bk@alpico.io>
2023-11-22 14:47:43 +0100

    cache: fix partial read case

--- ap-storage-memory/src/cache.rs
+++ ap-storage-memory/src/cache.rs
@@ -1,7 +1,10 @@
 use ap_storage::{Error, Offset, Read};
 
+/// A memory cache to speedup reads to an underlying disk.
+///
 /// TODO: LRU linked list, Multiple CacheSets
 pub struct MemoryCacheImpl<'a> {
+    /// The disk to cache the data for.
     parent: &'a dyn Read,
     /// The pages of user-data.
     userdata: &'a mut [u8],
@@ -14,18 +17,19 @@ pub struct MemoryCacheImpl<'a> {
 impl<'a> MemoryCacheImpl<'a> {
     const PAGE_SIZE: usize = 4096;
 
+    /// The data is used as backing store.
     pub fn new(data: &'a mut [u8], parent: &'a dyn Read) -> Self {
         let meta_size = core::mem::size_of::<Metadata>();
         let pages = data.len() / (Self::PAGE_SIZE + meta_size);
 
-        // we seperate the backing data here - the first is used for the pages, the remaining for the metadata
+        // we seperate the backing store here - the first is used for the pages, the remaining for the metadata
         let split = pages * Self::PAGE_SIZE;
         let meta = unsafe {
             core::slice::from_raw_parts_mut(data.as_mut_ptr().add(split) as *mut Metadata, pages)
         };
         let userdata = &mut data[..split];
 
-        // initialize the metadata
+        // initialize the metadata to point to nothing
         for entry in meta.iter_mut() {
             entry.page_offset = !0;
         }
@@ -38,6 +42,7 @@ impl<'a> MemoryCacheImpl<'a> {
         }
     }
 
+    /// Read from the disk. This requires a mutable self.
     pub fn read_mut(&mut self, ofs: Offset, buf: &mut [u8]) -> Result<usize, Error> {
         let page_offset = ofs / Self::PAGE_SIZE as Offset;
         let in_page = (ofs % Self::PAGE_SIZE as Offset) as usize;
@@ -64,19 +69,23 @@ impl<'a> MemoryCacheImpl<'a> {
         // try to read a full page
         let mut n = 0;
         while n != Self::PAGE_SIZE {
-            let c = self
+            let x = self
                 .parent
-                .read_bytes(ofs - (in_page + n) as Offset, &mut our[n..])?;
-            if c == 0 {
-                break;
+                .read_bytes(ofs - (in_page + n) as Offset, &mut our[n..]);
+            match x {
+                Err(_) if n > in_page => break,
+                Err(e) => return Err(e),
+                Ok(0) => break,
+                Ok(c) => { n+= c; }
             }
-            n += c;
         }
+
+        // only enable the cache if the whole page is filled
         if n == Self::PAGE_SIZE {
             self.meta[self.lru].page_offset = page_offset;
         }
-        let maxn = core::cmp::min(maxn, n);
-        buf[..maxn].copy_from_slice(&our[..maxn]);
+        let maxn = core::cmp::min(maxn, n - in_page);
+        buf[..maxn].copy_from_slice(&our[in_page..in_page + maxn]);
         Ok(maxn)
     }
 }
